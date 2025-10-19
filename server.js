1d377fe7f465a31b2ef5615b5aef33a26e9155e8
Bendy545 <130989123+Bendy545@users.noreply.github.com>
2025-10-19 16:42:50 +0200

    changes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const Post = mongoose.model('Post', postSchema, 'idnes');
 
 app.get('/posts', async (req, res) => {
     try {
-        const posts = await Post.find().sort({ Date: -1 }).limit(5);
+        const posts = await Post.find().sort({ Date: -1 }).limit(10);
         res.json(posts);
     } catch (err) {
         res.status(500).json({ message: err.message });
